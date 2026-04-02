518164883735fd6ce522f456e7f2a1dec992d5d5
shimotori <to_shimotori@tale-jp.com>
2026-04-02 15:38:40 +0900

    テストファイル削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import { Test } from "./test/Test";
-import { TestA } from "./test/TestA";
-import { TestB } from "./test/TestB";
-import { NotFound } from "./test/NotFound";
-import { Layout } from "./test/Layout";
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/t" replace />} />
-        <Route path="/t" element={<Layout />}>
-          <Route index element={<Test />} />
-          <Route path="test-a/:id" element={<TestA />} />
-          <Route path="test-b/:id" element={<TestB />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Router>
+    <p>aaa</p>
   );
 }
 
--- src/test/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-export const NotFound = () => {
-  return (
-    <>
-      <h1>404 Page Not Found</h1>
-    </>
-  );
-};
